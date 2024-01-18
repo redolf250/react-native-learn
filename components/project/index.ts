81e3fe3ba19b2f118d4ef2175bbba47e18d60ee2
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-01-18 15:55:31 +0000

    index to load images and keep code clean

---
+++ components/project/index.ts
@@ -0,0 +1,11 @@
+const IMAGES = {
+  IMAGE_1: require('./images/6.jpeg'),
+  IMAGE_2: require('./images/7.jpeg'),
+  IMAGE_3: require('./images/12.jpeg'),
+  IMAGE_4: require('./images/14.jpeg'),
+  IMAGE_5: require('./images/16.jpeg'),
+  IMAGE_6: require('./images/5.jpeg'),
+  IMAGE_7: require('./images/4.jpeg'),
+};
+
+export {IMAGES};
